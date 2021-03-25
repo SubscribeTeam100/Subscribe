a1c89b2d37b154570be2d51e4f961544b04cd15f
Nikhilesh Chhetri <chhetrinikhilesh@gmail.com>
2021-03-25 16:37:54 -0500

    login implemented

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -2,8 +2,46 @@ import React from 'react'
 
 
 function Search(){
+    const[errors, setErrors] = useState({
+
+    })
+    const [values, setValues] = useState({
+      
+      email:'',
+      
+      password: '',
+      
+    })
+    const onChange = (event) => {
+      setValues({...values,[event.target.name]:event.target.value})
+    }
+  
+    const [loginUser, {loading}] = useMutation(LOGIN_USER,{
+      update(proxy, result){
+        props.history.push('')
+      },
+      onError(err){
+        
+        setErrors(err.graphQLErrors[0].extensions.exception.errors)
+      
+      },
+      variables: values
+    })
+  
+    const onSubmit = (event)=>{
+      event.preventDefault();
+      
+      searchProducts();
+      
+    }  
+
+
     return (
-        <div>Searchpage</div>
+       <div>
+            <Form onSubmit={onSubmit} >
+            
+            </Form>
+       </div>
     )
 }
 
